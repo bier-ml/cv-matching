1f24f3aa8da91f4db6106945c0d897827b00501d
konstantgr <konstantin.grotov@gmail.com>
2023-12-22 16:03:19 +0300

    improved embeddings

--- core/embedding_models.py
+++ core/embedding_models.py
@@ -3,13 +3,21 @@ from typing import Iterable
 import numpy as np
 from sentence_transformers import SentenceTransformer
 
+AVAILABLE_EMBEDDINGS: set[str] = {
+    "cointegrated/rubert-tiny2",
+    "DeepPavlov/rubert-base-cased",
+    "DeepPavlov/bert-base-bg-cs-pl-ru-cased",
+    "DeepPavlov/rubert-base-cased-sentence",
+    "sentence-transformers/distiluse-base-multilingual-cased-v2",
+}
+
 
 class DummyEmbeddingModel:
     def __init__(self):
         np.random.seed(42)
 
     @staticmethod
-    def generate(text: str | list[str], dim: int = 312):
+    def generate(text: str | list[str], dim: int = 312) -> np.ndarray | Iterable[np.ndarray]:
         if isinstance(text, str):
             return np.random.rand(dim)
         else:
